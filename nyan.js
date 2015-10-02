a2b2d6eadeefde0847c08df16da30b96f3bb65c1
jimbonw <jimmehwhy@live.co.uk>
2015-10-02 21:19:22 +0000

    angry

--- nyan.js
+++ nyan.js
@@ -2,21 +2,21 @@
 
 if (Meteor.isClient) {
 
-// var canvas = document.getElementById('bird-canvas');
-// var context = canvas.getContext("2d");
+var canvas = document.getElementById('bird-canvas');
+var context = canvas.getContext("2d");
 
-// var color_array['red', 'yellow', 'orange', 'green', 'blue'];
+var color_array = ['red', 'yellow', 'orange', 'green', 'blue'];
 
-// var birdx = session.get('counter');
-// var birdy = 1;//erm;
-// var size = 10;
+var birdx = session.get('counter');
+var birdy = 1;//erm;
+var size = 10;
 
-// for(var i = 0; i < color_array.length; i++){
-//   //5 colors, size of rainbow square
-//   var nyansize = size / color_array.length;
+for(var i = 0; i < color_array.length; i++){
+  //5 colors, size of rainbow square
+  var nyansize = size / color_array.length;
 
-//   context.fillStyle = color_array[i];
-//   context.beginPath();
-//   context.rect(birdx, birdy - (nyansize * i), nyansize, nyansize);
-// }
+  context.fillStyle = color_array[i];
+  context.beginPath();
+  context.rect(birdx, birdy - (nyansize * i), nyansize, nyansize);
+}
 }
